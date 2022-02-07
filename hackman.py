8357b849245d6133c11f99e2717f4408fa77fb29
ModelT063 <jpittar@clemson.edu>
2022-02-07 09:10:29 -0500

    basic working game structure

--- hackman.py
+++ hackman.py
@@ -8,12 +8,12 @@ class hackman():
         self.userGuess = ""
         self.params = {
             "key": config.api_key,
-            "length": 6
+            "length": 15
         }
 
     # gets a word from API
     def callAPI(self):
-        response = requests.get("http://clemsonhackman.com/api/word", self.params)
+        response = requests.get("https://clemsonhackman.com/api/word", self.params)
         self.word = response.json()["word"]
         for x in range(len(self.word)):
             self.displayWord += "_"
@@ -25,12 +25,15 @@ class hackman():
         self.userGuess = ""
         self.callAPI()
     
-    # returns true if letter was correctly guessed     
+    # returns true if letter was correctly guessed/repeat
     def guessLetter(self, letter):
+        if self.userGuess.find(letter) != -1:
+            return True    
         self.userGuess += letter
         return self.updateDisplayWord(letter)
 
     def updateDisplayWord(self, letter):
+        # returns false if letter is incorrect
         index = self.word.find(letter)
         if index == -1:
             return False
@@ -39,8 +42,8 @@ class hackman():
         while index != -1:
             self.displayWord = self.displayWord[:index] + letter + self.displayWord[index + 1:]
             index = self.word.find(letter, index + 1)
-        print(self.displayWord)
         return True
-            
+    
+    # check win condition
     def win(self):
         return self.word == self.displayWord
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import hackman
+import random
 
 def main():
     pygame.init()
@@ -7,7 +8,11 @@ def main():
     pygame.display.set_caption("testing")
     screen = pygame.display.set_mode((800, 600))
     font = pygame.font.SysFont("monospace", 50)
+    timer = pygame.time.Clock()
+    display_time = 100
+    delta = 0
     game = hackman.hackman()
+    game.params["length"] = random.randint(4, 15)
     game.callAPI()
     
     running = True
@@ -16,18 +21,32 @@ def main():
         for event in pygame.event.get():
             # KEYBOARD PRESS EVENT
             if event.type == pygame.KEYDOWN:
-                # if user presses a letter, guess that letter ######### FIX THIS ALL EVENT KEYS RETURN ALPHA ################
-                if pygame.key.name(event.key)[-1].isalpha():
-                    game.guessLetter(pygame.key.name(event.key)[-1])
+                # if user presses a letter, guess that letter
+                if event.unicode.isalpha():
+                    if not game.guessLetter(event.unicode):
+                        display_time -= 10
+
 
             # CLOSE WINDOW EVENT
             elif event.type == pygame.QUIT:
                 running = False
-                
-        if game.win(): 
+        
+        if game.win() or display_time <= 0: 
+            display_time = 100
+            game.params["length"] = random.randint(4, 15)
             game.resetGame()
-        text = font.render(game.displayWord, False, (255,255,255))               
+        
+        # updates timer
+        delta += timer.tick()
+        if(delta > 1000):  
+            delta = 0
+            display_time -= 1
+
+        screen.fill((0,0,0))
+        text = font.render(game.displayWord, False, (255,255,255), (0,0,0))               
         screen.blit(text, (100, 100))
+        text = font.render(str(display_time), False, (255, 255, 255), (0,0,0))
+        screen.blit(text, (100, 200))
         pygame.display.update()
 
 if __name__ == "__main__":
